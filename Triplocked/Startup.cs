abf18bfc613a3b507b3a02688cb9b84c920613e6
Mateusz Kapiszewski <kapiszewski.mateusz@gmail.com>
2018-12-01 13:48:57 +0100

    First version with the communication, OnConnected, SendMessage, OnDisconnected methods

--- Triplocked/Startup.cs
+++ Triplocked/Startup.cs
@@ -6,40 +6,25 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using WebSocketManager;
 
 namespace Triplocked
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddWebSocketManager();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IServiceProvider serviceProvider)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseBrowserLink();
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-            }
+            app.UseWebSockets();
+            app.MapWebSocketManager("/game", serviceProvider.GetService<TriplockedCommunication>());
 
             app.UseStaticFiles();
-
-            app.UseMvc();
         }
     }
 }
---
+++ Triplocked/TriplockedCommunication.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading.Tasks;
+using WebSocketManager;
+using WebSocketManager.Common;
+
+namespace Triplocked
+{
+    public class TriplockedCommunication : WebSocketHandler
+    {
+        public TriplockedCommunication(WebSocketConnectionManager webSocketConnectionManager) : base(webSocketConnectionManager, new ControllerMethodInvocationStrategy())
+        {
+            ((ControllerMethodInvocationStrategy)MethodInvocationStrategy).Controller = this;
+        }
+
+        public override async Task OnConnected(WebSocket socket)
+        {
+            await base.OnConnected(socket);
+
+            var socketId = WebSocketConnectionManager.GetId(socket);
+
+            var message = new Message()
+            {
+                MessageType = MessageType.Text,
+                Data = $"{socketId} is now connected"
+            };
+
+            await SendMessageToAllAsync(message);
+        }
+
+        // this method can be called from a client, doesn't return anything.
+        public async Task SendMessage(WebSocket socket, string message)
+        {
+           await InvokeClientMethodToAllAsync("receiveMessage", WebSocketConnectionManager.GetId(socket), message);
+        }
+
+        public override async Task OnDisconnected(WebSocket socket)
+        {
+            var socketId = WebSocketConnectionManager.GetId(socket);
+
+            await base.OnDisconnected(socket);
+
+            var message = new Message()
+            {
+                MessageType = MessageType.Text,
+                Data = $"{socketId} disconnected"
+            };
+            await SendMessageToAllAsync(message);
+        }
+    }
+}
